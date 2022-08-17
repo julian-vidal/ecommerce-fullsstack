afc3609084e22dd084073614c04af49903d87aee
Julian Vidal <29507199+julian-vidal@users.noreply.github.com>
2022-08-17 17:28:10 -0500

    endpoint created for products and carts

--- src/server.js
+++ src/server.js
@@ -1,46 +1,216 @@
 // @ts-check
 
 "use strict";
-// Server setup
+/* ============================================
+Imports
+============================================*/
 const express = require("express");
+const Container = require("./utils/container");
+
+/* ============================================
+Server setup
+============================================*/
 const app = express();
 const PORT = 8080;
 const { Router } = express;
-const routerProducts = Router();
-const Container = require("./utils/container");
-const products = new Container("./products.json");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/products", routerProducts);
 app.use(express.static(__dirname + "/dist"));
 const server = app.listen(PORT, () => {
     console.log(`Server is listening at port ${PORT}`);
 });
 server.on("error", err => console.log());
 
+const IS_ADMIN = true;
+const PERMSSION_ERROR_MSG = "You don't have permission to perform this action";
+
+
+/* ============================================
+Carts
+============================================*/
+const routerCarts = Router();
+const carts = new Container("./carts.json");
+app.use("/api/carts", routerCarts);
+
+
+routerCarts.get("/", (req, res) => {
+    (async () =>{
+        try {
+            res.json(await carts.readFile());
+        }
+        catch (err) {
+            console.log(err);
+        }
+    })()    
+});
+
+routerCarts.get("/:id", (req, res) => {
+    (async () =>{
+        try {
+            const id = parseInt(req.params.id);
+            const response = await carts.getById(id, "cart")
+            res.json(response)
+        }
+        catch (err) {
+            console.log(err);
+        }
+    })()
+});
+
+
+routerCarts.post("/", (req,res) => {
+    try {
+        (async () => {
+            const response = await carts.save({products: []}, "cart");
+            res.json({response});
+        })()
+    } catch(error) {
+        res.json({error})
+    }
+})
+
+
+routerCarts.delete("/:id", (req,res) => {
+    try {
+        (async () => {
+            const id = parseInt(req.params.id);
+            const response = await carts.deleteById(id, "cart");
+            res.json({response})
+        })()
+    } catch (error) {
+        res.json({error})
+    }
+})
+
+
+routerCarts.get("/:id/products", (req, res) => {
+    (async () =>{
+        try {
+            const id = parseInt(req.params.id);
+            const response = await carts.getById(id, "cart")
+            res.json(response.products)
+        }
+        catch (err) {
+            console.log(err);
+        }
+    })()
+});
+
+
+routerCarts.post("/:id/products", (req,res) => {
+    try {
+        (async () => {
+            const id = parseInt(req.params.id);
+            const product = req.body;
+            let cart = await carts.getById(id, "cart")
+            const response = await carts.addProductToCart(product, id, cart)
+            res.json({response});
+        })()
+    } catch(error) {
+        res.json({error})
+    }
+})
+
+routerCarts.delete("/:id/products/:idProd", (req,res) => {
+    try {
+        (async () => {
+            const idCart = parseInt(req.params.id);
+            const idProd = parseInt(req.params.idProd);
+            const cart = await carts.getById(idCart, "cart");
+
+            const response = await carts.deleteProductFromCart(idCart, idProd, cart);
+            res.json({response})
+        })()
+    } catch (error) {
+        res.json({error})
+    }
+})
+
+/* ============================================
+Products
+============================================*/
+const routerProducts = Router();
+const products = new Container("./products.json");
+app.use("/api/products", routerProducts);
 
 
 routerProducts.get("/", (req, res) => {
-    try {
-        (async () => {
+    (async () =>{
+        try {
             res.json(await products.readFile());
-        })()
-    }
-    catch (err) {
-        console.log(err);
-    }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    })()    
+});
+
+
+routerProducts.get("/:id", (req, res) => {
+    (async () =>{
+        try {
+            const id = parseInt(req.params.id);
+            const response = await products.getById(id, "product")
+            res.json(response)
+        }
+        catch (err) {
+            console.log(err);
+        }
+    })()
 });
 
 
 routerProducts.post("/", (req,res) => {
-    try {
-        const product = req.body;
-        product.price = parseFloat(product.price);
-        product.stock = parseInt(product.stock);
-        product.timestamp = Date.now();
-        products.save(product);
-        res.json(product);
-    } catch(error) {
-        res.json({error})
+    if (IS_ADMIN) {
+        try {
+            (async () => {
+                const product = req.body;
+                product.price = parseFloat(product.price);
+                product.stock = parseInt(product.stock);
+                const response = await products.save(product, "product");
+                res.json({response});
+            })()
+        } catch(error) {
+            res.json({error})
+        }
+    } else {
+        res.json({error: PERMSSION_ERROR_MSG})
     }
+})
+
+routerProducts.put("/:id", (req,res) => {
+    if (IS_ADMIN) {
+        try{
+            (async () => {
+                const id = parseInt(req.params.id);
+                let product = req.body;
+                product.id = id;
+                product.price = parseFloat(product.price);
+                product.stock = parseInt(product.stock);
+                const response = await products.updateById(product, id, "product");
+                res.json({response});
+            })()
+        } catch(error) {
+            res.json({error})
+        }
+    } else {
+        res.json({error: PERMSSION_ERROR_MSG})
+    }      
+})
+
+routerProducts.delete("/:id", (req,res) => {
+    if (IS_ADMIN) {
+        try {
+            (async () => {
+                const id = parseInt(req.params.id);
+                const response = await products.deleteById(id, "product");
+                res.json({response})
+            })()
+        } catch (error) {
+            res.json({error})
+        }
+    } else {
+        res.json({error: PERMSSION_ERROR_MSG})
+    }
+
 })
--- src/utils/container.js
+++ src/utils/container.js
@@ -33,31 +33,156 @@ class Container {
         }
     }
 
-    async save(newData) {
-        try {
-            let newID;
-            let positionId;
-            let data = await this.readFile();
-
-            if (data.length == 0 || !data) {
-                newID = 1;
-            } else {
-                newID = data[data.length-1].id + 1;
-                positionId = await this.checkIfIdExists(data, newID);
-                while(positionId != -1) {
-                    newID = newID+1;
-                    positionId = await this.checkIfIdExists(data, newID);
-                }
+    async assignId(array) {
+        let newId;
+        if (array.length == 0 || !array) {
+            newId = 1;
+        } else {
+            newId = array[array.length-1].id + 1;
+            let newPositionId = await this.checkIfIdExists(array, newId);
+            while (newPositionId != -1) {
+                newId = newId + 1;
+                newPositionId = await this.checkIfIdExists(array, newId);
             }
+        }
+        console.log(`new ID: ${newId}`)
+        return(newId)
+    }
 
-            newData.id = newID;
+    async save(newData, objectType) {
+        try {
+            
+            let data = await this.readFile();
+            newData.id = await this.assignId(data)
+            newData.timestap = Date.now()
             data.push(newData);
 
             await fs.promises.writeFile(this.fileName, JSON.stringify(data));
-            console.log(`A new product with ID ${newID} has been added`);
-
+            const response = `A new ${objectType} with ID ${newData.id} has been added`
+            console.log(response);
+            return response
         } catch( err) {
             console.log(err)
+        }
+    }
+
+    async deleteById(id, objectType) {
+        try {
+            let data = await this.readFile();
+            let positionId = await this.checkIfIdExists(data,id);
+            let response;
+            if (positionId == -1) {
+                response = `There's no a ${objectType} with ID ${id}`;
+                console.log(response)
+                return response
+            } else {
+                data.splice(positionId,1);
+                await fs.promises.writeFile(this.fileName, JSON.stringify(data));
+                response = `The ${objectType} with ID ${id} has been deleted`
+                console.log(response)
+                return response
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async updateById(newData, id, objectType) {
+        try {
+            let data = await this.readFile();
+            let positionId = await  this.checkIfIdExists(data,id);
+            let response;
+            if (positionId == -1) {
+                response = `There's no a ${objectType} with ID ${id}`
+                console.log(response)
+                return ({response: response})
+            } else {
+                newData.timestap = Date.now();
+                data.splice(positionId, 1, newData);
+                await fs.promises.writeFile(this.fileName, JSON.stringify(data));
+                response = `The ${objectType} with ID ${id} has been updated`
+                console.log(response);
+                return response;
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    async getById(id, objectType) {
+        try {
+            let data = await this.readFile();
+            let positionId = await this.checkIfIdExists(data,id);
+            let response;
+
+            if(positionId == -1) {
+                response = `There's no a ${objectType} with ID ${id}`
+                console.log(response)
+                return {response}
+            } else {
+                response = data[positionId];
+                console.log(response);
+                return response
+            }
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    // Exclusive for the cart endpoint
+
+    async addProductToCart(newProduct, cartId, cart) {
+        try {
+            let data = await this.readFile();
+            let positionId = await  this.checkIfIdExists(data,cartId);
+            let response;
+            
+            if (positionId == -1) {
+                response = `There's no a cart with ID ${cartId}`
+                console.log(response)
+                return (response)
+            } else {
+                newProduct.id = await this.assignId(cart.products);
+                newProduct.timestap = Date.now();
+                cart.products.push(newProduct)
+                data.splice(positionId, 1, cart);
+                await fs.promises.writeFile(this.fileName, JSON.stringify(data));
+                response = `The cart with ID ${cartId} has been updated`
+                console.log(response);
+                return response;
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    async deleteProductFromCart(idCart, idProd, cart) {
+        try {
+            let data = await this.readFile();
+            const positionIdCart = await this.checkIfIdExists(data,idCart);
+            let response;
+
+            if (positionIdCart == -1) {
+                response = `There's no a cart with ID ${idCart}`;
+                console.log(response)
+                return response
+            } else {
+
+                const positionIdProduct = await this.checkIfIdExists(cart.products, idProd);
+
+                if (positionIdProduct == -1) {
+                    response = `There's no a product with ID ${idProd} at the cart ID ${idCart}`;
+                    console.log(response)
+                    return response
+                } else {
+                    cart.products.splice(positionIdProduct,1);
+                    response = await this.updateById(cart, idCart, "cart")
+                    console.log(response)
+                    return response
+                }
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 }
